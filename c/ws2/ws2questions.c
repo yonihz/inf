b2b517d56fb1dfb4874a0bf958e6bb83c2cfbe0e
yoni.horovitz <yonihz@gmail.com>
2019-08-06 19:35:30 -0400

    ws2

---
+++ c/ws2/ws2questions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	/*
+	unsigned int* ip = 0;
+	float f = 3;
+	float *fp = &f;
+	ip = (unsigned int*) fp;
+	printf("%u\n", *ip);
+	*/
+
+	int* p = (int*)17;
+	printf("%d\n", (int)(long)(p+4));
+
+	return (0);
+}
