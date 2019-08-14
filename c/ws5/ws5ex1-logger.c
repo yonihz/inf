cf71bf848c08b38c76fbeddacc5c162baf3f8f30
yoni.horovitz <yonihz@gmail.com>
2019-08-14 15:12:19 +0300

    ws5

--- c/ws5/ws5ex1-logger.c
+++ c/ws5/ws5ex1-logger.c
@@ -2,13 +2,14 @@
 #include <string.h> /* for strlen, strcmp, strncmp */
 #include <stdlib.h> /* for malloc */
 
-#define SIZE_ARR 4
+#define SIZE_ARR 5 /* struct array size, defined by number of operations types */
 #define UNUSED(x) (void)(x)
 
+/* file pointer declaration */
 FILE *file_ptr;
 
 /* enum definition */
-enum op_status {PASS = 0, FAIL = 1};
+enum op_status {PASS = 0, FAIL = 1, EXIT = 2};
 
 /* operations functions */
 enum op_status AppendStr(char* filename, char* input_str);
@@ -18,19 +19,19 @@ enum op_status CountLines(char* filename, char* input_str);
 enum op_status ExitProgram(char* filename, char* input_str);
 
 /* comparison functions */
-int CmpAllChars(const char* dest, const char* src, size_t len);
-int CmpNChars(const char* dest, const char* src, size_t len);
+int CmpAllChars(const char* dest, const char* src);
+int Cmp1Char(const char* dest, const char* src);
+int CmpNull(const char* dest, const char* src);
 
 int main(int argc, char** argv)
 {
-	size_t len = 0;
 	int i = 0;
-	char* filename1 = 0;
+	int status = 0;
 	char input_str[1000];
 	
 	/* typedef of pointers to functions */
 
-	typedef int (*f_cmp_ptr_t)(const char*, const char*, size_t);	
+	typedef int (*f_cmp_ptr_t)(const char*, const char*);	
 	typedef enum op_status (*f_op_ptr_t)(char*, char*);
 
 	/* struct includes command, pointers to comparison and operation functions */
@@ -40,24 +41,29 @@ int main(int argc, char** argv)
 		char* cmd_str;
 		f_cmp_ptr_t f_cmp_ptr;
 		f_op_ptr_t f_op_ptr;
-	};
+	};	
 
 	/* init struct array for each operation function */
 
 	struct logger arr [SIZE_ARR] = { 
-	{"-remove", &CmpAllChars, &RemoveFile}, 
-	{"-count", &CmpAllChars, &CountLines}, 
 	{"-exit", &CmpAllChars, &ExitProgram}, 
-	{"<", &CmpNChars, &PrependStr}
+	{"-count", &CmpAllChars, &CountLines}, 
+	{"-remove", &CmpAllChars, &RemoveFile}, 
+	{"<", &Cmp1Char, &PrependStr},
+	{"", &CmpNull, &AppendStr}
 	};
-		
-	UNUSED(argc);
 
-	len = strlen(argv[1]) + 1; /* find length of filename string, malloc and strcpy from argv */
-	filename1 = malloc(len);
-	strcpy(filename1, argv[1]);
+	/* check if user entered a filename */
 
-	while (1)
+	if (argc != 2)
+	{
+		printf("Error: filename missing. Exiting program\n");
+		return (0);
+	}	
+
+	/* main loop for operations */
+
+	while (status != EXIT)
 	{
 		printf("Enter a string:\n");
 
@@ -65,29 +71,30 @@ int main(int argc, char** argv)
 
 		input_str[strcspn(input_str, "\n")] = 0; /* remove newline char from string */
 
-		len = strlen(input_str);
-
 		for (i = 0; i < SIZE_ARR; i++)
 		{
-			if (arr[i].f_cmp_ptr(input_str,arr[i].cmd_str,len) == 0)
+			if (arr[i].f_cmp_ptr(input_str,arr[i].cmd_str) == 0)
 			{	
-				arr[i].f_op_ptr(filename1, input_str);
+				status = arr[i].f_op_ptr(argv[1], input_str);
+				printf("\n");
 				break;
 			}
 		}
-		
-		if (i == SIZE_ARR)
+
+		if (status == FAIL)
 		{
-			AppendStr(filename1, input_str);
+			printf("Error. Exiting program\n");
+			return (0);
 		}
 	}
 
 	return (0);
 }
 
+/* Line count function */
+
 enum op_status CountLines(char* filename, char* input_str)
-{
-	int status = PASS;	
+{	
 	int count = 0;
 	char c = 0;
 
@@ -97,10 +104,11 @@ enum op_status CountLines(char* filename, char* input_str)
 	
 	if (file_ptr == NULL)
 	{
-		printf("Error in file open\n");
-		status = FAIL;
-		return(status);
+		printf("Error: no file\n");
+		return(PASS);
 	}
+
+	rewind(file_ptr);
 
 	c = getc(file_ptr);
 
@@ -115,62 +123,149 @@ enum op_status CountLines(char* filename, char* input_str)
 	}
 
 	printf("Number of lines is %d\n\n", count);
-	return (status);
+	return (PASS);
 }
+
+/* Prepend function */
 
 enum op_status PrependStr(char* filename, char* input_str)
 {
-	int status = PASS;
+	FILE* file_ptr_temp;
+	int file_exists = 1;
+	char c = 0;
 
-	UNUSED(input_str);
+	printf("OPERATION: Prepend\n");
 
-	printf("OPERATION: Prepend\n\n");
-	return (status);
+	file_ptr = fopen(filename,"r");
+
+	if (file_ptr != NULL)
+	{
+		file_exists = 0;
+		fclose(file_ptr);
+	}
+
+	if (file_exists != 0)
+	{
+		file_ptr = fopen(filename,"a");
+		input_str += 1;
+		fputs(input_str,file_ptr);
+		fputs("\n",file_ptr);
+		fclose(file_ptr);
+
+		return (PASS);
+	}
+
+	file_ptr = fopen(filename,"r");
+
+	file_ptr_temp = fopen("temp","w");
+
+	input_str += 1;
+	fputs(input_str, file_ptr_temp);
+	fputs("\n", file_ptr_temp);
+
+	rewind(file_ptr);
+
+	c = fgetc(file_ptr);
+
+	while (c != EOF)
+	{	
+		fputc(c, file_ptr_temp);
+		c = fgetc(file_ptr);
+	}
+	
+	fputs("\n",file_ptr);
+
+	fclose(file_ptr_temp);
+
+	fclose(file_ptr);
+
+	remove(filename);
+	
+	rename("temp", filename);
+
+	return (PASS);
 }
+
+/* Append function */
 
 enum op_status AppendStr(char* filename, char* input_str)
 {
-	int status = PASS;
+	printf("OPERATION: Append\n");
 
-	UNUSED(input_str);
+	file_ptr = fopen(filename,"a");
+	
+	if (file_ptr == NULL)
+	{
+		printf("Error in file open\n");
+		return(FAIL);
+	}
 
-	printf("OPERATION: Append\n\n");
-	return (status);
+	fputs(input_str,file_ptr);
+	fputs("\n",file_ptr);
+
+	fclose(file_ptr);
+
+	return (PASS);
 }
+
+/* Remove file function */
 
 enum op_status RemoveFile(char* filename, char* input_str)
 {
-	int status = PASS;
+	int status;
 
 	UNUSED(input_str);
-
+		
 	printf("OPERATION: Remove file\n\n");
+
+	file_ptr = fopen(filename,"r");
+	
+	if (file_ptr == NULL)
+	{
+		printf("Error: no file\n");
+		return(PASS);
+	}
+
+	/* remove function returns 0 if successful */
+
+	status = remove(filename);
+
 	return (status);
 }
 
+/* Exit program function */
+
 enum op_status ExitProgram(char* filename, char* input_str)
 {
-	int status = PASS;
-
 	UNUSED(input_str);
+	UNUSED(filename);
 
-	printf("Exit\n");
-	exit(status);
+	printf("Exiting program\n");
+
+	return (EXIT);
 }
 
-int CmpAllChars(const char* dest, const char* src, size_t len)
+/* Compare string to all chars using strcmp function */
+
+int CmpAllChars(const char* dest, const char* src)
 {
-	UNUSED(len);
 	return (strcmp(dest, src));
 }
 
-int CmpNChars(const char* dest, const char* src, size_t len)
-{
-	if (*src == '<')
-	{
-		len = 1;
-	}
+/* Compare string to N chars using strncmp function */
 
-	return (strncmp(dest, src, len));
+int Cmp1Char(const char* dest, const char* src)
+{
+	return (strncmp(dest, src, 1));
+}
+
+/* Null compare function */
+
+int CmpNull(const char* dest, const char* src)
+{
+	UNUSED(dest);
+	UNUSED(src);
+
+	return (0);
 }
 
