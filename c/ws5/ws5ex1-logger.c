5ae56e63f96e8c22feea27646ebb9670132a6cc6
yoni.horovitz <yonihz@gmail.com>
2019-08-14 10:33:50 +0300

    ws5

--- c/ws5/ws5ex1-logger.c
+++ c/ws5/ws5ex1-logger.c
@@ -1,22 +1,28 @@
 #include <stdio.h> /* for file functions */
-#include <string.h> /* for strlen, strcmp */
+#include <string.h> /* for strlen, strcmp, strncmp */
 #include <stdlib.h> /* for malloc */
 
 #define SIZE_ARR 4
 #define UNUSED(x) (void)(x)
 
-int AppendStr(FILE* file_ptr, char* input_str);
-int SeekStart(FILE* file_ptr);
-int RemoveFile(FILE* file_ptr);
-int CountLines(FILE* file_ptr);
-int Exit(FILE* file_ptr);
+FILE *file_ptr;
 
-enum return_op {PASS = 0, FAIL = 1};
- 
+/* enum definition */
+enum op_status {PASS = 0, FAIL = 1};
+
+/* operations functions */
+enum op_status AppendStr(char* filename, char* input_str);
+enum op_status PrependStr(char* filename, char* input_str);
+enum op_status RemoveFile(char* filename, char* input_str);
+enum op_status CountLines(char* filename, char* input_str);
+enum op_status ExitProgram(char* filename, char* input_str);
+
+/* comparison functions */
+int CmpAllChars(const char* dest, const char* src, size_t len);
+int CmpNChars(const char* dest, const char* src, size_t len);
+
 int main(int argc, char** argv)
 {
-	FILE *file_ptr;
-
 	size_t len = 0;
 	int i = 0;
 	char* filename1 = 0;
@@ -24,8 +30,8 @@ int main(int argc, char** argv)
 	
 	/* typedef of pointers to functions */
 
-	typedef int (*f_cmp_ptr_t)(const char*, const char*);	
-	typedef int (*f_op_ptr_t)(FILE*);
+	typedef int (*f_cmp_ptr_t)(const char*, const char*, size_t);	
+	typedef enum op_status (*f_op_ptr_t)(char*, char*);
 
 	/* struct includes command, pointers to comparison and operation functions */
 
@@ -39,10 +45,10 @@ int main(int argc, char** argv)
 	/* init struct array for each operation function */
 
 	struct logger arr [SIZE_ARR] = { 
-	{"-remove", &strcmp, &RemoveFile}, 
-	{"-count", &strcmp, &CountLines}, 
-	{"-exit", &strcmp, &Exit}, 
-	{"<", &strcmp, &SeekStart}
+	{"-remove", &CmpAllChars, &RemoveFile}, 
+	{"-count", &CmpAllChars, &CountLines}, 
+	{"-exit", &CmpAllChars, &ExitProgram}, 
+	{"<", &CmpNChars, &PrependStr}
 	};
 		
 	UNUSED(argc);
@@ -59,39 +65,42 @@ int main(int argc, char** argv)
 
 		input_str[strcspn(input_str, "\n")] = 0; /* remove newline char from string */
 
+		len = strlen(input_str);
+
 		for (i = 0; i < SIZE_ARR; i++)
 		{
-			file_ptr = fopen(filename1,"w");
-
-			if (arr[i].f_cmp_ptr(input_str,arr[i].cmd_str) == 0)
+			if (arr[i].f_cmp_ptr(input_str,arr[i].cmd_str,len) == 0)
 			{	
-				if (file_ptr == NULL)
-				{
-					printf("Error in file\n");
-					exit(FAIL);
-				}				
-
-				arr[i].f_op_ptr(file_ptr);
-
+				arr[i].f_op_ptr(filename1, input_str);
 				break;
 			}
 		}
 		
-		if (i == SIZE_ARR || i == 3)
+		if (i == SIZE_ARR)
 		{
-			AppendStr(file_ptr, input_str);
+			AppendStr(filename1, input_str);
 		}
-
-		fclose(file_ptr);
 	}
 
 	return (0);
 }
 
-int CountLines(FILE* file_ptr)
+enum op_status CountLines(char* filename, char* input_str)
 {
-	char c = 0;
+	int status = PASS;	
 	int count = 0;
+	char c = 0;
+
+	UNUSED(input_str);
+
+	file_ptr = fopen(filename,"r");
+	
+	if (file_ptr == NULL)
+	{
+		printf("Error in file open\n");
+		status = FAIL;
+		return(status);
+	}
 
 	c = getc(file_ptr);
 
@@ -106,33 +115,62 @@ int CountLines(FILE* file_ptr)
 	}
 
 	printf("Number of lines is %d\n\n", count);
+	return (status);
 }
 
-int SeekStart(FILE* file_ptr)
+enum op_status PrependStr(char* filename, char* input_str)
 {
-	fseek(file_ptr, 0, SEEK_SET);
+	int status = PASS;
+
+	UNUSED(input_str);
+
 	printf("OPERATION: Prepend\n\n");
+	return (status);
 }
 
-int AppendStr(FILE* file_ptr, char* input_str)
+enum op_status AppendStr(char* filename, char* input_str)
 {
-	int status = FAIL;
-	printf("status = %d\n", status);
-	fputs(input_str, file_ptr);	
-	fseek(file_ptr, 0, SEEK_END);
-	fputc(10, file_ptr);
+	int status = PASS;
+
+	UNUSED(input_str);
+
 	printf("OPERATION: Append\n\n");
+	return (status);
 }
 
-int RemoveFile(FILE* file_ptr)
+enum op_status RemoveFile(char* filename, char* input_str)
 {
-	int status;
+	int status = PASS;
+
+	UNUSED(input_str);
+
 	printf("OPERATION: Remove file\n\n");
+	return (status);
 }
 
-int Exit(FILE* file_ptr)
+enum op_status ExitProgram(char* filename, char* input_str)
 {
+	int status = PASS;
+
+	UNUSED(input_str);
+
 	printf("Exit\n");
-	exit(PASS);
+	exit(status);
+}
+
+int CmpAllChars(const char* dest, const char* src, size_t len)
+{
+	UNUSED(len);
+	return (strcmp(dest, src));
+}
+
+int CmpNChars(const char* dest, const char* src, size_t len)
+{
+	if (*src == '<')
+	{
+		len = 1;
+	}
+
+	return (strncmp(dest, src, len));
 }
 
