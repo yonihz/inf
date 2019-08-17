49bd5680ef0c26a70af6550d4cec9734c2f33510
yoni.horovitz <yonihz@gmail.com>
2019-08-17 19:27:56 +0300

    ws3

--- c/ws3/ws3ex2-josephus.c
+++ c/ws3/ws3ex2-josephus.c
@@ -2,6 +2,7 @@
 #include <stdlib.h> /* for calloc(), free() */
 
 int Josephus(int size, int first);
+int NextAlive(int i, int size, int arr[]);
 
 int main( int argc, char *argv[] )
 {
@@ -11,7 +12,19 @@ int main( int argc, char *argv[] )
 	(void)argc;
 	(void)argv;
 
+	if (2 != argc)
+	{
+		printf("Error: please enter one argument (integer > 1)\n");
+		return (0);
+	}	
+
 	size = atoi(argv[1]);
+
+	if (1 >= size)
+	{
+		printf("Error: please enter an integer > 1\n");
+		return (0);
+	}	
 
 	printf("Number of soldiers = %d\n", size);
 
@@ -23,42 +36,40 @@ int main( int argc, char *argv[] )
 int Josephus(int size, int first)
 {
 	int i = first;
-	int last_stabber = first;
+	int last_stabber = -1;
 	int* arr = 0;
 
 	arr = calloc(size, sizeof(int));
 
-	do
+	while (last_stabber != i)
 	{
 			last_stabber = i;
-			
-			do /* find next soldier to be stabbed */
-			{
-				if (i == size-1)
-				{
-					i = 0;
-					continue;
-				}
-			
-				i++;
-			}
-			while (arr[i] == 1);
+
+			i = NextAlive(i, size, arr); /* find next soldier to be stabbed */
 
 			arr[i] = 1;
 
-			do /* find next soldier alive */
-			{
-				if (i == size-1)
-				{
-					i = 0;
-					continue;
-				}
-			
-				i++;
-			}
-			while (arr[i] == 1);
+			i = NextAlive(i, size, arr); /* find next soldier alive */	
 	}
-	while (last_stabber != i);
-	
+
+	free(arr);
+
 	return i;
+}
+
+int NextAlive(int i, int size, int arr[])
+{
+	do 
+	{
+		if (i == size-1)
+		{
+			i = 0;
+			continue;
+		}
+			
+		i++;
+	}
+	while (arr[i] == 1);
+
+	return (i);
 }
