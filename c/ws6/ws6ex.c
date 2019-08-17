38872430fa468195dbdf766ae5fe493266cd5f55
yoni.horovitz <yonihz@gmail.com>
2019-08-17 11:42:08 +0300

    ws6

--- c/ws6/ws6ex.c
+++ c/ws6/ws6ex.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 
-/* macros for lookup table (8 bits) */
+/* macros for set bits LUT (8 bits) */
 #define B2(n) n, n + 1, n + 1, n + 2
 #define B4(n) B2(n), B2(n + 1), B2(n + 1), B2(n + 2)
 #define B6(n) B4(n), B4(n + 1), B4(n + 1), B4(n + 2)
-#define COUNT_BITS B6(0), B6(1), B6(1), B6(2)
+#define SET_BITS B6(0), B6(1), B6(1), B6(2)
 
-/* lookup table - each index has the corresponding number of bits */
-unsigned int lut[256] = { COUNT_BITS };
+unsigned int set_bits_lut[256] = { SET_BITS };
 
-long pow2(unsigned int x, unsigned int y);
+/* macros for reverese bits LUT (8 bits) */
+
+#define R2(n)     n,     n + 2*64,     n + 1*64,     n + 3*64
+#define R4(n) R2(n), R2(n + 2*16), R2(n + 1*16), R2(n + 3*16)
+#define R6(n) R4(n), R4(n + 2*4 ), R4(n + 1*4 ), R4(n + 3*4 )
+#define REVERSE_BITS R6(0), R6(2), R6(1), R6(3)
+
+unsigned int rev_bits_lut[256] = { REVERSE_BITS };
+
+long x2powy(unsigned int x, unsigned int y);
 int isPow2Loop(unsigned int n);
 int isPow2NoLoop(unsigned int n);
 unsigned int Add1(unsigned int n);
 void PrintIf3Bits(unsigned int n[], size_t size);
 int is3Bits(unsigned int n);
-unsigned int ByteMirrorLoop(unsigned int n);
-unsigned int ByteMirrorNoLoop(unsigned int n);
+unsigned char ByteMirrorLoop(unsigned char c);
+unsigned char ByteMirrorLUT(unsigned char c);
 unsigned char CheckBits2OR6(unsigned char c);
 unsigned char CheckBits2AND6(unsigned char c);
 unsigned char SwapBits3And5(unsigned char c);
@@ -32,7 +40,7 @@ int main()
 	unsigned int x = 3;
 	unsigned int y = 4;
 	unsigned int n = 16;
-	unsigned char c = 33;
+	unsigned char c = 128;
 	unsigned int arr[20];
 	size_t size_arr = 0;
 	float f = -1.2;
@@ -45,43 +53,44 @@ int main()
 	}
 
 	/* ex1 test: x * (2 ^ y) */
-	printf("%d * (2 ^ %d) = %ld\n", x, y, pow2(x,y));
+	printf(" 1) x*(2^y): %d * (2 ^ %d) = %ld\n", x, y, x2powy(x,y));
 
 	/* ex2 test: isPow2 */
-	printf("isPow2Loop(%u) = %d\n", n, isPow2Loop(n));
-	printf("isPow2NoLoop(%u) = %d\n", n, isPow2NoLoop(n));
+	printf(" 2) isPow2Loop(%u) = %d\n", n, isPow2Loop(n));
+	printf(" 3) isPow2NoLoop(%u) = %d\n", n, isPow2NoLoop(n));
 
 	/* ex3 test: add 1 */
-	printf("Add1(%u) = %u\n", n, Add1(n));
+	printf(" 4) Add1(%u) = %u\n", n, Add1(n));
 
 	/* ex4 test: print if n has 3 bits on */
 	PrintIf3Bits(arr,size_arr);
 
 	/* ex5 test: byte mirror */
-	printf("ByteMirrorLoop(%d) = %d\n", n, ByteMirrorLoop(n));
+	printf(" 6) ByteMirrorLoop(%u) = %u\n", c, ByteMirrorLoop(c));
+	printf(" 7) ByteMirrorLUT(%u) = %u\n", c, ByteMirrorLUT(c));
 
 	/* ex6 test: check if bits 2 and 6 are on */
-	printf("CheckBits2AND6(%d) = %d\n", c, CheckBits2AND6(c));
+	printf(" 8) CheckBits2AND6(%d) = %d\n", c, CheckBits2AND6(c));
 
 	/* ex7 test: check if bits 2 or 6 are on */
-	printf("CheckBits2OR6(%d) = %d\n", c, CheckBits2OR6(c));
+	printf(" 9) CheckBits2OR6(%d) = %d\n", c, CheckBits2OR6(c));
 
 	/* ex8 test: swap bits 3 and 5 */
-	printf("SwapBits3And5(%d) = %d\n", c, SwapBits3And5(c));
+	printf("10) SwapBits3And5(%d) = %d\n", c, SwapBits3And5(c));
 
 	/* ex9 test: find closest num dividable by 16 */
-	printf("ClosestDiv16(%d) = %d\n", n, ClosestDiv16(n));
+	printf("11) ClosestDiv16(%d) = %d\n", n, ClosestDiv16(n));
 
 	/* ex10 test: find closest num dividable by 16 */
-	printf("Before Swap: n1 = %u, n2 = %u\n", x, y);
+	printf("12) Before Swap: n1 = %u, n2 = %u | ", x, y);
 	Swap(&x, &y);
 	printf("After Swap: n1 = %u, n2 = %u\n", x, y);
 
 	/* ex11 test: count set bits loop */
-	printf("CountSetBits(%u) = %d\n", n, CountSetBitsLoop(n));
+	printf("13) CountSetBits(%u) = %d\n", n, CountSetBitsLoop(n));
 
 	/* ex12 test: count set bits loop */
-	printf("CountSetBitsLUT(%u) = %d\n", n, CountSetBitsLUT(n));
+	printf("14) CountSetBitsLUT(%u) = %d\n", n, CountSetBitsLUT(n));
 
 	/* ex13 test: prints bits of float */
 	PrintBitsFloat(f);
@@ -91,7 +100,7 @@ int main()
 
 /* ex1 function: x * (2 ^ y) */
 
-long pow2(unsigned int x, unsigned int y)
+long x2powy(unsigned int x, unsigned int y)
 {
 	return x << y;
 }
@@ -155,7 +164,7 @@ void PrintIf3Bits(unsigned int n[], size_t size)
 {
 	size_t i = 0;
 
-	printf("PrintIf3Bits: ");
+	printf(" 5) PrintIf3Bits: ");
 
 	for (i = 0; i < size; i++)
 	{
@@ -191,37 +200,39 @@ int is3Bits(unsigned int n)
 	return (0);
 }
 
-/* ex5-1 function: byte mirror (with loop) */
+/* ex5-1 function: byte mirror (loop) */
 
-unsigned int ByteMirrorLoop(unsigned int n)
+unsigned char ByteMirrorLoop(unsigned char c)
 {
-	unsigned int m1 = 1, m2 = 1 << 7;
-	unsigned int n_m1 = 0, n_m2 = 0;
-	unsigned int i = 0;
-	unsigned int mirror = 0;
+	unsigned int mirror = c;	
 	unsigned int size = 8;
+	unsigned int i = 0;
 
-	for (i = 0; i < (size / 2); i++)
+	while (i < (size - 1))
 	{
-		n_m1 = m1 & n;
-		n_m1 <<= ((size - 1) - 2 * i);
-		mirror |= n_m1;
-		m1 <<= 1;
-
-		n_m2 = m2 & n;
-		n_m2 >>= (2 * i + 1);
-		mirror |= n_m2;
-		m2 >>= 1;
+		mirror <<= 1;
+		c >>= 1;
+		mirror |= c & 1;
+		i++;
 	}
 
 	return (mirror);
 }
 
-/* ex5-2 function: byte mirror (no loop) */
+/* ex5-2 function: byte mirror (LUT) */
 
-unsigned int ByteMirrorNoLoop(unsigned int n)
+unsigned char ByteMirrorLUT(unsigned char c)
 {
-	return (0);
+	c = rev_bits_lut[c];
+
+	/*
+	n = (rev_bits_lut[n & 0xff] << 24) | 
+		(rev_bits_lut[(n >> 8) & 0xff] << 16) | 
+		(rev_bits_lut[(n >> 16) & 0xff] << 8) |
+		(rev_bits_lut[(n >> 24) & 0xff]);
+	*/
+	
+	return (c);
 }
 
 /* ex6 function: check if bits 2 and 6 are on */
@@ -326,10 +337,10 @@ int CountSetBitsLUT(unsigned int n)
 {
 	int count = 0; 
 
-	count = lut[n & 0xff] +
-			lut[n >> 8 & 0xff] +
-			lut[n >> 16 & 0xff] +
-			lut[n >> 24];
+	count = set_bits_lut[n & 0xff] +
+			set_bits_lut[n >> 8 & 0xff] +
+			set_bits_lut[n >> 16 & 0xff] +
+			set_bits_lut[n >> 24];
 
 	return (count);
 }
@@ -363,6 +374,8 @@ void PrintBitsFloat(float f)
 		
 	bit = (f < 0) ? 1 : 0;
 	a[i] = bit;
+
+	printf("15) Binary of float %f is: ", f);
 
 	for (i = 0; i < 32; i++)
 	{
