884188929468fea299256aa7cec067c211eff356
yoni.horovitz <yonihz@gmail.com>
2019-08-31 17:42:59 +0300

    ds vector

--- ds/test/vector_test.c
+++ ds/test/vector_test.c
@@ -29,18 +29,26 @@
 
 #ifndef NDEBUG
 void TestVectorInvalidPtr();
+void TestVectorSize0();
 #endif
 
 /* API test functions */
 
 void TestVectorCreate();
+void TestVectorGrow();
+void TestVectorGetItem();
+void TestVectorReserve();
 
 int main()
 {
 	TestVectorCreate();
+	TestVectorGrow();
+	TestVectorGetItem();
+	TestVectorReserve();
 
 #ifndef NDEBUG
-/*	TestVectorInvalidPtr(); */
+/*	TestVectorInvalidPtr(); */  
+/*	TestVectorSize0(); */
 #endif
     
 	return (0);
@@ -51,24 +59,24 @@ int main()
 void VerifyVectort(vector_t* test, vector_t* expected, char test_name[])
 {
 	(test == expected) ?
-	(printf(GRN), printf("%s # TEST PASS #\n", test_name)) :
-	(printf(RED), printf("%s # TEST FAIL #\n", test_name)) ;
+	(printf(GRN), printf("## TEST PASS ## %s\n", test_name)) :
+	(printf(RED), printf("## TEST FAIL ## %s\n", test_name)) ;
 	printf(RESET);
 }
 
 void VerifyInt(int test, int expected, char test_name[])
 {
 	(test == expected) ?
-	(printf(GRN), printf("%s # TEST PASS #\n", test_name)) :
-	(printf(RED), printf("%s # TEST FAIL #\n", test_name)) ;
+	(printf(GRN), printf("## TEST PASS ## %s\n", test_name)) :
+	(printf(RED), printf("## TEST FAIL ## %s\n", test_name)) ;
 	printf(RESET);
 }
 
 void VerifySizet(size_t test,size_t expected, char test_name[])
 {
 	(test == expected) ?
-	(printf(GRN), printf("%s # TEST PASS #\n", test_name)) :
-	(printf(RED), printf("%s # TEST FAIL #\n", test_name)) ;
+	(printf(GRN), printf("## TEST PASS ## %s\n", test_name)) :
+	(printf(RED), printf("## TEST FAIL ## %s\n", test_name)) ;
 	printf(RESET);
 }
 
@@ -77,22 +85,23 @@ void VerifyVoidptr(void* test, void* expected, char test_name[])
 	if (NULL == expected)
 	{
 		(test == NULL) ?
-		(printf(GRN), printf("%s # TEST PASS #\n", test_name)) :
-		(printf(RED), printf("%s # TEST FAIL #\n", test_name)) ;
+		(printf(GRN), printf("## TEST PASS ## %s\n", test_name)) :
+		(printf(RED), printf("## TEST FAIL ## %s\n", test_name)) ;
+		printf(RESET);
 		return;
 	}
 
 	(*(int*)test == *(int*)expected) ?
-	(printf(GRN), printf("%s # TEST PASS #\n", test_name)) :
-	(printf(RED), printf("%s # TEST FAIL #\n", test_name)) ;
+	(printf(GRN), printf("## TEST PASS ## %s\n", test_name)) :
+	(printf(RED), printf("## TEST FAIL ## %s\n", test_name)) ;
 	printf(RESET);
 }
 
 void VerifyChar(char test[], char expected[], char test_name[])
 {
 	(!strcmp(test, expected)) ?
-	(printf(GRN), printf("%s # TEST PASS #\n", test_name)) :
-	(printf(RED), printf("%s # TEST FAIL #\n", test_name)) ;
+	(printf(GRN), printf("## TEST PASS ## %s\n", test_name)) :
+	(printf(RED), printf("## TEST FAIL ## %s\n", test_name)) ;
 	printf(RESET);
 }
 
@@ -101,15 +110,205 @@ void VerifyChar(char test[], char expected[], char test_name[])
 void TestVectorCreate()
 {
 	vector_t* vector1 = NULL;
-    size_t num_of_elements = 5;
-    size_t size_of_elements = sizeof(int);
-    int a[5] = {0, 1, 2, 3, 4};
+    size_t num_of_elements = 8;
+    size_t size_of_element = sizeof(int);
+    int a[8] = {0, 1, 2, 3, 4, 5, 6, 7};
 
 	printf("VectorCreate Tests\n");
-    vector1 = VectorCreate(num_of_elements, size_of_elements);
+    vector1 = VectorCreate(size_of_element, num_of_elements);
 
-    VerifyInt(VectorPushBack(vector1, (a+3)), 1,
+    VerifyInt(VectorPushBack(vector1, (a+3)), 0,
     "TEST 1 - VECTOR CREATE AND PUSH");
+
+	VectorDestroy(vector1);
+}
+
+void TestVectorGrow()
+{
+	vector_t* vector1 = NULL;
+    size_t num_of_elements = 4;
+    size_t size_of_element = sizeof(int);
+    int a[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+
+	printf("Grow and Shrink Tests\n");
+    vector1 = VectorCreate(size_of_element, num_of_elements);
+
+    VerifySizet(VectorCapacity(vector1), 4,
+    "TEST 1 - CAPACITY IS 4 AFTER CREATE WITH 4 ELEMENTS");
+
+	VectorPushBack(vector1, (a+0));
+	VectorPushBack(vector1, (a+1));
+	VectorPushBack(vector1, (a+2));
+	VectorPushBack(vector1, (a+3));
+
+    VerifySizet(VectorCapacity(vector1), 4,
+    "TEST 1 - CAPACITYIS 4 AFTER 4 PUSH");
+    VerifySizet(VectorSize(vector1), 4,
+    "TEST 2 - SIZE IS 4 AFTER 4 PUSH");
+
+	VectorPushBack(vector1, (a+4));
+
+    VerifySizet(VectorCapacity(vector1), 8,
+    "TEST 3 - CAPACITY IS 8 AFTER 5 PUSH");
+
+    VerifySizet(VectorSize(vector1), 5,
+    "TEST 4 - SIZE IS 5 AFTER 5 PUSH");
+
+	VectorPushBack(vector1, (a+5));
+	VectorPushBack(vector1, (a+6));
+	VectorPushBack(vector1, (a+7));
+	VectorPushBack(vector1, (a+8));
+
+    VerifySizet(VectorCapacity(vector1), 16,
+    "TEST 5 - CAPACITY IS 16 AFTER 9 PUSH");
+
+    VerifySizet(VectorSize(vector1), 9,
+    "TEST 6 - SIZE IS 9 AFTER 9 PUSH");
+
+	VectorPopBack(vector1);
+	VectorPopBack(vector1);
+	VectorPopBack(vector1);
+	VectorPopBack(vector1);
+
+    VerifySizet(VectorCapacity(vector1), 16,
+    "TEST 7 - CAPACITY IS 16 AFTER 4 POP");
+
+    VerifySizet(VectorSize(vector1), 5,
+    "TEST 8 - SIZE IS 5 AFTER 4 PUSH");
+
+	VectorPopBack(vector1);
+
+    VerifySizet(VectorCapacity(vector1), 8,
+    "TEST 9 - CAPACITY IS 8 AFTER 1 POP");
+
+    VerifySizet(VectorSize(vector1), 4,
+    "TEST 10 - SIZE IS 4 AFTER 1 POP");
+
+	VectorPopBack(vector1);
+
+    VerifySizet(VectorCapacity(vector1), 8,
+    "TEST 11 - CAPACITY IS 8 AFTER 1 POP");
+
+    VerifySizet(VectorSize(vector1), 3,
+    "TEST 12 - SIZE IS 3 AFTER 1 POP");
+
+	VectorPopBack(vector1);
+
+    VerifySizet(VectorCapacity(vector1), 4,
+    "TEST 13 - CAPACITY IS 4 AFTER 1 POP");
+
+    VerifySizet(VectorSize(vector1), 2,
+    "TEST 14 - SIZE IS 2 AFTER 1 POP");
+
+	VectorPopBack(vector1);
+
+    VerifySizet(VectorCapacity(vector1), 2,
+    "TEST 15 - CAPACITY IS 2 AFTER 1 POP");
+
+    VerifySizet(VectorSize(vector1), 1,
+    "TEST 16 - SIZE IS 1 AFTER 1 POP");
+
+	VectorPopBack(vector1);
+
+    VerifySizet(VectorCapacity(vector1), 2,
+    "TEST 17 - CAPACITY IS 2 AFTER 1 POP");
+
+    VerifySizet(VectorSize(vector1), 0,
+    "TEST 18 - SIZE IS 0 AFTER 1 POP");
+
+	VectorPopBack(vector1);
+
+    VerifySizet(VectorCapacity(vector1), 2,
+    "TEST 19 - CAPACITY IS 2 AFTER 1 POP");
+
+    VerifySizet(VectorSize(vector1), 0,
+    "TEST 20 - SIZE IS 0 AFTER 1 POP");
+
+	VectorDestroy(vector1);
+}
+
+void TestVectorGetItem()
+{
+	vector_t* vector1 = NULL;
+    size_t num_of_elements = 4;
+    size_t size_of_element = sizeof(int);
+    int a[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+
+	printf("Push Pop GetItem Tests\n");
+    vector1 = VectorCreate(size_of_element, num_of_elements);
+
+    VerifyVoidptr(VectorGetItemAddress(vector1, 0), NULL,
+    "TEST 1 - GET ITEM IS NULL AFTER CREATE");
+
+	VectorPushBack(vector1, (a+0));
+	VectorPushBack(vector1, (a+1));
+	VectorPushBack(vector1, (a+2));
+	VectorPushBack(vector1, (a+3));
+	VectorPushBack(vector1, (a+4));
+
+    VerifyVoidptr(VectorGetItemAddress(vector1, 0), (a+0),
+    "TEST 2 - GET ITEM 0 AFTER 5 PUSH");
+    VerifyVoidptr(VectorGetItemAddress(vector1, 1), (a+1),
+    "TEST 3 - GET ITEM 1 AFTER 5 PUSH");
+    VerifyVoidptr(VectorGetItemAddress(vector1, 2), (a+2),
+    "TEST 4 - GET ITEM 2 AFTER 5 PUSH");
+    VerifyVoidptr(VectorGetItemAddress(vector1, 3), (a+3),
+    "TEST 5 - GET ITEM 3 AFTER 5 PUSH");
+    VerifyVoidptr(VectorGetItemAddress(vector1, 4), (a+4),
+    "TEST 6 - GET ITEM 4 AFTER 5 PUSH");
+
+    VerifyVoidptr(VectorGetItemAddress(vector1, 5), NULL,
+    "TEST 7 - GET ITEM OUT OF RANGE AFTER 5 PUSH");
+
+	VectorPopBack(vector1);
+	VectorPopBack(vector1);
+	VectorPopBack(vector1);
+	VectorPopBack(vector1);
+
+    VerifyVoidptr(VectorGetItemAddress(vector1, 0), (a+0),
+    "TEST 8 - GET ITEM 0 AFTER 5 PUSH");
+
+	VectorPopBack(vector1);
+
+    VerifyVoidptr(VectorGetItemAddress(vector1, 0), NULL,
+    "TEST 9 - GET ITEM 0 AFTER VECTOR IS EMPTY");
+
+	VectorDestroy(vector1);
+}
+
+void TestVectorReserve()
+{
+	vector_t* vector1 = NULL;
+    size_t num_of_elements = 4;
+    size_t size_of_element = sizeof(int);
+    int a[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+
+	printf("Reserve Tests\n");
+    vector1 = VectorCreate(size_of_element, num_of_elements);
+
+    VerifySizet(VectorCapacity(vector1), 4,
+    "TEST 1 - CAPACITY IS 4 AFTER CREATE WITH 4 ELEMENTS");
+
+	VectorReserve(vector1, 6);
+
+    VerifySizet(VectorCapacity(vector1), 6,
+    "TEST 2 - CAPACITY IS 6 AFTER RESERVE WITH 6 ELEMENTS");
+
+	VectorPushBack(vector1, (a+0));
+	VectorPushBack(vector1, (a+1));
+	VectorPushBack(vector1, (a+2));
+	VectorPushBack(vector1, (a+3));
+	VectorPushBack(vector1, (a+4));
+	VectorPushBack(vector1, (a+5));
+	VectorPushBack(vector1, (a+6));
+
+    VerifySizet(VectorCapacity(vector1), 12,
+    "TEST 3 - CAPACITY IS 12 AFTER 7 PUSH");
+
+	VectorReserve(vector1, 14);
+
+    VerifySizet(VectorCapacity(vector1), 14,
+    "TEST 4 - CAPACITY IS 14 AFTER RESERVE WITH 14 ELEMENTS");
 
 	VectorDestroy(vector1);
 }
@@ -120,6 +319,27 @@ void TestVectorCreate()
 
 void TestVectorInvalidPtr()
 {
+	vector_t* vector1 = NULL;
+    size_t num_of_elements = 4;
+    size_t size_of_element = sizeof(int);
+    int a[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 
+	printf("VectorDestroy Debug Tests - INVALID POINTER\n");
+    vector1 = VectorCreate(size_of_element, num_of_elements);
+	VectorPushBack(vector1, (a+0));
+
+	VectorDestroy(vector1);
+	VectorDestroy(vector1);
+}
+
+void TestVectorSize0()
+{
+	vector_t* vector1 = NULL;
+    size_t size_of_element = sizeof(int);
+    int a[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+
+	printf("VectorCreate Debug Tests - SIZE 0 AND PUSH\n");
+    vector1 = VectorCreate(size_of_element, 0);
+	VectorPushBack(vector1, (a+0));
 }
 #endif
--- ds/vector.c
+++ ds/vector.c
@@ -23,6 +23,7 @@
 
 #ifndef NDEBUG
 #define ACTIVE 0xDEADBEEF
+#define ALLOCATED 0xBADDCAFE
 #endif
 
 struct vector
@@ -50,14 +51,13 @@ vector_t *VectorCreate(size_t size_of_element, size_t num_of_elements)
     new_vector->size_of_element = size_of_element;
 	new_vector->head = NULL;
 	new_vector->current = NULL;
+	new_vector->end = NULL;
 
 #ifndef NDEBUG
     new_vector->status = ACTIVE;
 #endif
 
 	VectorReserve(new_vector, num_of_elements);
-
-	assert(NULL != new_vector->head);
 
     return (new_vector);
 }
@@ -68,29 +68,40 @@ int VectorReserve(vector_t* vector, size_t num_of_elements)
 	size_t current_size = 0;
 	size_t total_size = vector->size_of_element * num_of_elements;
 
-	assert(NULL != vector || ACTIVE == vector->status);
+	if (0 == total_size)
+	{
+		return (0);
+	}
+
+	assert(NULL != vector &&
+	(ACTIVE == vector->status || ALLOCATED == vector->status));
 
 	if (NULL != vector->head)
 	{
-		current_size = VectorSize(vector);
+		current_size = VectorSize(vector) * vector->size_of_element;
 	}
 
 	temp_head = realloc(vector->head, total_size);
 
 	if (NULL == temp_head)
 	{
-		return (0);
+		return (1);
 	}
+
+#ifndef NDEBUG
+    vector->status = ALLOCATED;
+#endif
 		
 	vector->head = temp_head;
 	vector->current = (char*)vector->head + current_size;
     vector->end = (char*)vector->head + total_size;
-	return (1);
+	return (0);
 }
 
 void VectorDestroy(vector_t* vector)
 {
-	assert(NULL != vector || ACTIVE == vector->status);
+	assert(NULL != vector &&
+	(ACTIVE == vector->status || ALLOCATED == vector->status));
 
 #ifndef NDEBUG
     vector->status = 0x00000000;
@@ -102,7 +113,8 @@ void VectorDestroy(vector_t* vector)
 
 size_t VectorSize(const vector_t* vector)
 {
-	assert(NULL != vector || ACTIVE == vector->status);
+	assert(NULL != vector &&
+	(ACTIVE == vector->status || ALLOCATED == vector->status));
 	
 	return
 	(((char*)vector->current - (char*)vector->head)/vector->size_of_element);
@@ -110,7 +122,8 @@ size_t VectorSize(const vector_t* vector)
 
 size_t VectorCapacity(const vector_t* vector)
 {
-	assert(NULL != vector || ACTIVE == vector->status);
+	assert(NULL != vector &&
+	(ACTIVE == vector->status || ALLOCATED == vector->status));
 	
 	return
 	(((char*)vector->end - (char*)vector->head)/vector->size_of_element);
@@ -118,28 +131,26 @@ size_t VectorCapacity(const vector_t* vector)
 
 int VectorPushBack(vector_t* vector, const void *element)
 {
-	int status = 1;
+	int status = 0;
 
-	assert(NULL != vector || ACTIVE == vector->status);
+	assert(NULL != vector && ALLOCATED == vector->status);
 
 	if (vector->current == vector->end)
 	{
 		status = VectorReserve(vector, (VectorCapacity(vector) * 2));
 	}
-	if (0 == status)
-	{
-		return (0);
-	}
+
+	assert(0 == status);
 	
 	memcpy(vector->current, element, vector->size_of_element);
 
 	vector->current = (char*)vector->current + vector->size_of_element;
-	return (1);
+	return (status);
 }
 
 void VectorPopBack(vector_t* vector)
 {
-	assert(NULL != vector || ACTIVE == vector->status);
+	assert(NULL != vector && ALLOCATED == vector->status);
 
 	if (vector->current == vector->head)
     {
@@ -148,7 +159,8 @@ void VectorPopBack(vector_t* vector)
 
 	vector->current = (char*)vector->current - vector->size_of_element;
 
-	if (VectorSize(vector) <= (VectorCapacity(vector) / 4))
+	if ((VectorSize(vector) <= (VectorCapacity(vector) / 4)) &&
+		VectorSize(vector) != 0)
 	{
 		VectorReserve(vector, (VectorCapacity(vector) / 2));
 	}
@@ -157,9 +169,10 @@ void VectorPopBack(vector_t* vector)
 
 void *VectorGetItemAddress(const vector_t* vector, size_t index)
 {
-	assert(NULL != vector || ACTIVE == vector->status);
+	assert(NULL != vector && ALLOCATED == vector->status);
 
-	if (vector->current == vector->head)
+	if ( (vector->current == vector->head) ||
+		((index + 1) > VectorSize(vector)) )
     {
         return (NULL);
     }
