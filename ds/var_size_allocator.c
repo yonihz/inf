4b9adb237ba389ca0fa298ffa5e77a715801a9f7
yoni.horovitz <yonihz@gmail.com>
2019-09-28 01:01:03 +0300

    ds vsa

--- ds/var_size_allocator.c
+++ ds/var_size_allocator.c
@@ -51,71 +51,52 @@ vsa_t* VSAInit(void* seg, size_t total_size)
 
 void* VSAAlloc(vsa_t* vsa, size_t block_size)
 {
-    node_vsa_t *curr_node = NULL, *next_node = NULL;
-    ptrdiff_t word_size = sizeof(size_t);
-    ptrdiff_t nwords_block = 0, nwords_node = 0, nbytes_count = 0;
-    ptrdiff_t nwords_req = 0, nwords_left;
+    node_vsa_t *curr = NULL, *next = NULL;
+    ptrdiff_t word = sizeof(size_t);
+    ptrdiff_t wblock = 0, wnode = 0, wcount = 0;
 
     if (0 == block_size)
     {
         return (NULL);
     }
   
-    nwords_block = ((word_size - block_size % word_size) % word_size
-                    + block_size) / word_size;
-        
-    nwords_node = sizeof(node_vsa_t) / word_size;
-    curr_node = (node_vsa_t*)((vsa_t*)vsa + 1);
-    nbytes_count = labs(curr_node->offset) + nwords_node;
+    wblock = ((word - block_size % word) % word + block_size) / word; 
+    wnode = sizeof(node_vsa_t) / word;
+    curr = (node_vsa_t*)((vsa_t*)vsa + 1);
+    wcount = labs(curr->offset) + wnode;
 
-    while (nbytes_count < vsa->total_size)
+    while (wcount < vsa->total_size)
     {
-        next_node = (node_vsa_t*)((ptrdiff_t*)curr_node + nwords_node + curr_node->offset);
+        next = (node_vsa_t*)((ptrdiff_t*)curr + wnode + curr->offset);
         
-        if (next_node->offset < 0)
+        if (next->offset < 0)
         {
-            curr_node = next_node;
-            nbytes_count += (-1) * curr_node->offset + nwords_node;
+            curr = next;
+            wcount += (-1) * curr->offset + wnode;
             continue;
         }
 
-        nwords_req = nwords_block - curr_node->offset;
-        nwords_left = next_node->offset + nwords_node - nwords_req;
-        nwords_left = nwords_left > 0 ? nwords_left : 0;
-
-        if (nwords_left <= nwords_node)
+        curr->offset += next->offset + wnode;
+        wcount += curr->offset + wnode;
+        if (wblock <= curr->offset)
         {
-            curr_node->offset += next_node->offset + nwords_node;
-        }
-        else
-        {
-            curr_node->offset += nwords_req;
-            next_node = (node_vsa_t*)((ptrdiff_t*)curr_node + nwords_node + curr_node->offset);
-            next_node->offset = nwords_left - nwords_node; 
-        }
-        nbytes_count += curr_node->offset + nwords_node;
-
-        if (curr_node->offset >= nwords_block)
-        {
-            curr_node->offset *= (-1);
-            return (curr_node + 1);
+            break;
         }
     }
 
-    if (curr_node->offset >= nwords_block)
+    if (wblock > curr->offset)
     {
-        if (curr_node->offset > nwords_node)
-        {
-            nwords_left = curr_node->offset - nwords_block;
-            curr_node->offset = nwords_block;
-            next_node = (node_vsa_t*)((ptrdiff_t*)curr_node + nwords_node + curr_node->offset);
-            next_node->offset = nwords_left - nwords_node; 
-        }
-        curr_node->offset *= (-1);
-        printf("alloc: curr_node->offset %ld\n", curr_node->offset);
-        return (curr_node + 1);
+        return (NULL);
     }
-    return (NULL);
+
+    if (wblock < (curr->offset - wnode))
+    {
+        next = (node_vsa_t*)((ptrdiff_t*)curr + wnode + wblock + curr->offset);
+        next->offset = curr->offset - wblock - wnode;
+        curr->offset = wblock;
+    }
+    curr->offset *= (-1);
+    return (curr + 1);
 }
 
 void VSAFree(void *block)
