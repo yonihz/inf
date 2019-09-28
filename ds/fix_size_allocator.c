b17558d13e0d919810bf0a08ba0d86ff4b6a097e
yoni.horovitz <yonihz@gmail.com>
2019-09-28 18:17:04 +0300

    ds fsa

--- ds/fix_size_allocator.c
+++ ds/fix_size_allocator.c
@@ -1,53 +1,92 @@
+#include <stdio.h>
+
+#include "fix_size_allocator.h"
 
 struct allocator
 {
-    size_t num_of_blocks;
-    size_t size_block;
-    size_t size_free;
     void** next_free;
+    size_t block_size;
+    size_t nblocks;
+    size_t nfree;
+};
+
+size_t FSASuggestSize(size_t block_size, size_t num_of_blocks)
+{
+    size_t word = 0, meta_size = 0, suggest_size = 0;
+    
+    word = sizeof(size_t);
+    meta_size = word;
+    block_size += (word - block_size % word) % word;
+    suggest_size = (block_size + meta_size) * num_of_blocks + 
+                    sizeof(allocator_t) + (word - 1);
+    return (suggest_size);
 }
 
-/*O() 
-block_size - the size of each unit in bytes
-num_of_block - the max num of memomry blocks
-return- the total size is needed to allocate by the user in bytes
-*/
-size_t FSASuggestSize(size_t block_size, size_t num_of_blocks);
-
-/*O(n)
-dest- the begining of the segment that allocated by the user
-block_size- the size of each unit in bytes
-total_size- the total size allocated by the user in bytes
-return- the iter of the memory management unit
-the begining of the segment that allocated. return null if the total size too small
-the return address and dest can be diffrent! be carefull when you free the segment
-*/
 allocator_t* FSAInit(void* dest, size_t block_size, size_t total_size)
 {
+    size_t word = 0, meta_size = 0, dest_tail = 0, i = 0, ablock_size = 0;
+    void** node = NULL;
+    allocator_t* alloc = NULL;
 
+    word = sizeof(size_t);
+    meta_size = word;
+    dest_tail = (word - (size_t)dest % word) % word;
+    dest = (void*)((char*)dest + dest_tail);
+    ablock_size = block_size + (word - block_size % word) % word;
+
+    alloc = (allocator_t*)dest;
+    alloc->nblocks = (total_size - dest_tail - sizeof(allocator_t)) / 
+                        (ablock_size + meta_size);
+    alloc->block_size = block_size;
+    alloc->nfree = alloc->nblocks;
+    alloc->next_free = (void*)(alloc + 1);
+
+    node = alloc->next_free;
+    for (i = 0; i < alloc->nblocks; i++)
+    {
+        *node = (void*)((char*)node + ablock_size + meta_size);
+        node = *node;
+    }
+
+    return (alloc);
 }
 
-/* O(1)
-allocator- in which segment do allocation
-return- the address of the allocated block, if full return NULL
-*/ 
-void* FSAAlloc(allocator_t *allocator);
+void* FSAAlloc(allocator_t *allocator)
+{
+    void* block = NULL;
+    void** meta_block = NULL;
 
-/*O(1)
-block- the address of the block to free
-*/
-void FSAFree(void *block);
+    if (allocator->nfree == 0)
+    {
+        return (NULL);
+    }
 
-/*O(1)
-allocator- which segment
-return num of free blocks */
-size_t FSACountFree(const allocator_t *allocator);
+    meta_block = allocator->next_free;
+    block = (void*)(meta_block + 1);
+    allocator->next_free = *(meta_block);
+    *(meta_block) = &(allocator->next_free);
+    allocator->nfree -= 1;
+    return (block);
+}
 
-/*O(1)
-allocator- which segment
-return the size of each block */
+void FSAFree(void *block)
+{
+    void** meta_block = NULL;
+    allocator_t* alloc = NULL;
+
+    meta_block = (void**)block - 1;
+    alloc = (allocator_t*)(*meta_block);
+    *meta_block = alloc->next_free;
+    alloc->next_free = meta_block;
+    alloc->nfree +=1;
+}
+
+size_t FSACountFree(const allocator_t *allocator)
+{
+    return (allocator->nfree);
+}
+
 size_t FSAGetBlockSize(const allocator_t *allocator)
 {
-    return (allocator->size_block);
+    return (allocator->block_size);
 }
-
--- ds/var_size_allocator.c
+++ ds/var_size_allocator.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "var_size_allocator.h"
+
+#define VALID 0xDEADBEEF
 
 /* Offset convention: used (-) / free (+), type is ptrdiff_t */
 struct node_vsa
 {
     ptrdiff_t offset;
 #ifndef NDEBUG
-    ptrdiff_t magic;
+    size_t magic;
 #endif
 };
 
@@ -45,6 +48,7 @@ vsa_t* VSAInit(void* seg, size_t total_size)
     vsa = (vsa_t*)seg;
     vsa->total_size = nwords_free;
     first_node = (node_vsa_t*)((vsa_t*)seg + 1);
+    first_node->magic = VALID;
     first_node->offset = nwords_free - nwords_node;
     return (seg);
 }
@@ -54,21 +58,15 @@ void* VSAAlloc(vsa_t* vsa, size_t block_size)
     node_vsa_t *curr = NULL, *next = NULL;
     ptrdiff_t word = sizeof(size_t);
     ptrdiff_t wblock = 0, wnode = 0, wcount = 0;
-
-    if (0 == block_size)
-    {
-        return (NULL);
-    }
   
     wblock = ((word - block_size % word) % word + block_size) / word; 
     wnode = sizeof(node_vsa_t) / word;
     curr = (node_vsa_t*)((vsa_t*)vsa + 1);
     wcount = labs(curr->offset) + wnode;
 
-    while (wcount < vsa->total_size)
+    while (wcount < vsa->total_size && wblock > curr->offset)
     {
         next = (node_vsa_t*)((ptrdiff_t*)curr + wnode + curr->offset);
-        
         /* if next space is used, continue to next */
         if (next->offset < 0)
         {
@@ -76,24 +74,15 @@ void* VSAAlloc(vsa_t* vsa, size_t block_size)
             wcount += (-1) * curr->offset + wnode;
             continue;
         }
-
         /* defrag current and next spaces */
         curr->offset += next->offset + wnode;
         wcount += curr->offset + wnode;
-
-        /* if block has enough free space, break */
-        if (wblock <= curr->offset)
-        {
-            break;
-        }
     }
-
     /* return NULL if not enough space was found */
-    if (wblock > curr->offset)
+    if (wblock > curr->offset || 0 == block_size)
     {
         return (NULL);
     }
-
     /* if enough free space, create another free space */ 
     if (wblock < (curr->offset - wnode))
     {
@@ -102,6 +91,7 @@ void* VSAAlloc(vsa_t* vsa, size_t block_size)
         curr->offset = wblock;
     }
     curr->offset *= (-1);
+    curr->magic = VALID;
     return (curr + 1);
 }
 
@@ -114,14 +104,12 @@ void VSAFree(void *block)
         return;
     }
 
-    node_tofree = (node_vsa_t*)((char*)block - sizeof(node_vsa_t));
+    node_tofree = (node_vsa_t*)block - 1;
+
+    assert(VALID == node_tofree->magic);
 
 #ifndef NDEBUG
-    if (0xDEADBEEF == node_tofree->magic)
-    {
-        return;
-    }
-   node_tofree->magic = 0xDEADBEEF; 
+   node_tofree->magic = 0; 
 #endif
 
     node_tofree->offset *= (-1);
@@ -149,9 +137,7 @@ size_t VSAGetLargestBlock(const vsa_t* vsa)
             continue;
         }
         curr->offset = next->offset + wnode;
-
         largest = largest >= curr->offset ? largest : curr->offset; 
     }
     return (largest * word);
 }
-
