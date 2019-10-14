1d78ff7f0e1a676be3d8be30681870fe91f197ad
yoni.horovitz <yonihz@gmail.com>
2019-10-14 23:44:53 +0300

    ds bst

--- ds/include/bst.h
+++ ds/include/bst.h
@@ -5,35 +5,37 @@
 /* 13.10.19  13:00 */
 #include <stddef.h> /* size_t */
 
-typedef struct bst *bst_t;
+typedef struct bst bst_t;
 typedef struct bst_node *bst_itr_t;
 
-/* returns 0 if data1 is before data2, and returns 1 if equal or data1 is after data2*/
+/* returns 0 if data1 is before data2 */
+/* returns 1 if data1 is after or equal to data2 */
 typedef int(*cmp_func_t)(const void *data1, const void *data2, void *param);
 
-/* returns 0 if operation succesful, otherwise returns non-zero */
+/* returns 0 if operation successful, otherwise returns non-zero */
 typedef int(*op_func_t)(void *data, void *param);
 
 /* O(1) */
-/* returns NULL if failed */
+/* returns NULL on error */
 bst_t *BSTCreate(cmp_func_t cmp_func, void *param);
 
 /* O(n) */
 void BSTDestroy(bst_t *bst);
 
-/* Average: O(log(n)) Worst-case: O(n) */
-/* return itr of match, otherwise returns BSTEnd(bst) if no match */
+/* O(log(n)) / O(n) */
+/* return itr of match, otherwise returns BSTEnd(bst) if no match found */
 bst_itr_t BSTFind(bst_t *bst, const void *data);
 
-/* Average: O(log(n)) Worst-case: O(n) */
-/* returns itr of inserted data, otherwise returns BSTEnd(bst) if failed */
+/* O(log(n)) / O(n) */
+/* returns itr of inserted data, otherwise returns BSTEnd(bst) on error */
 bst_itr_t BSTInsert(bst_t *bst, void *data);
 
-/* Average: O(log(n)) Worst-case: O(n) */
+/* O(log(n)) / O(n) */
 void BSTRemove(bst_itr_t itr);
 
 /* O(n) */
-/* range is [itr_start, itr_end). returns last op_func return value */
+/* op_func range: [itr_start, itr_end) */
+/* returns last op_func return value */
 int BSTForEach(bst_itr_t itr_start, bst_itr_t itr_end, op_func_t op_func, void *param);
 
 /* O(n) */
@@ -45,18 +47,18 @@ int BSTIsEmpty(const bst_t *bst);
 /* O(1) */
 void *BSTGetData(bst_itr_t itr);
 
-/* Average: O(log(n)) Worst-case: O(n) */
+/* O(log(n)) / O(n) */
 /* no special cases */
 bst_itr_t BSTNext(bst_itr_t itr);
 
-/* Average: O(log(n)) Worst-case: O(n) */
+/* O(log(n)) / O(n) */
 /* no special cases */
 bst_itr_t BSTPrev(bst_itr_t itr);
 
-/* Average: O(log(n)) Worst-case: O(n) */
+/* O(log(n)) / O(n) */
 bst_itr_t BSTBegin(const bst_t *bst);
 
-/* Average: O(log(n)) Worst-case: O(n) */
+/* O(log(n)) / O(n) */
 bst_itr_t BSTEnd(const bst_t *bst);
 
 /* O(1) */
