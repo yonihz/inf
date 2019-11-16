5c343e0cddee27a1b9f51c79090abf9d9d922995
yoni.horovitz <yonihz@gmail.com>
2019-11-16 10:27:14 +0200

    dhcp

--- ds/dhcp.c
+++ ds/dhcp.c
@@ -1,6 +1,5 @@
-#include <stdlib.h>
+#include <stdlib.h> /* malloc */
 #include <assert.h>
-#include <stdio.h>
 
 #include "dhcp.h"
 
@@ -17,7 +16,6 @@ static int DHCPCountChildren(dhcp_node_t *node);
 static size_t RecSize(dhcp_node_t *node, unsigned int height);
 static ip_t GetBroadcastIP(ip_t net_addr, size_t prefix);
 static ip_t GetServerIP(ip_t net_addr);
-static void PrintIP(ip_t ip);
 
 struct dhcp_node {
     dhcp_node_t *child[2];
@@ -49,11 +47,8 @@ dhcp_t *DHCPCreate(ip_t net_addr, size_t prefix)
 
     /* BC: xxx.xxx.xxx.255, Server: xxx.xxx.xxx.1, Network: xxx.xxx.xxx.0 */
 
-    printf("Alloc NET IP:\n");
     status |= DHCPAlloc(dhcp, net_addr, &out_ip);
-    printf("Alloc BC IP:\n");
     status |= DHCPAlloc(dhcp, GetBroadcastIP(net_addr, prefix), &out_ip);
-    printf("Alloc SERVER IP:\n");
     status |= DHCPAlloc(dhcp, GetServerIP(net_addr), &out_ip);
 
     if (0 != status)
@@ -80,9 +75,6 @@ dhcp_ret_val_t DHCPAlloc(dhcp_t *dhcp, ip_t req_ip, ip_t *out_ip)
     int status = SUCCESS;
     unsigned int host = 0, host_mask = 0, host_bit_range = 0;
 
-    printf("req_ip: ");
-    PrintIP(req_ip);
-
     if (0 != dhcp->root->is_full)
     {
         return (NO_FREE_IPS);
@@ -102,9 +94,6 @@ dhcp_ret_val_t DHCPAlloc(dhcp_t *dhcp, ip_t req_ip, ip_t *out_ip)
 
     *out_ip = dhcp->net_addr + host;
 
-    printf("out_ip: ");
-    PrintIP(*out_ip);
-
     return (status);
 }
 
@@ -120,7 +109,7 @@ dhcp_ret_val_t DHCPFree(dhcp_t *dhcp, ip_t ip)
     }
 
     host_bit_range = IP4_BIT_RANGE - dhcp->prefix;
-    host_mask = (1 << (host_bit_range - 1)) - 1;
+    host_mask = (1 << host_bit_range) - 1;
     host = ip & host_mask;
     RecFree(dhcp->root, &status, &host, host_bit_range);
 
@@ -160,7 +149,6 @@ static dhcp_node_t *RecAlloc(dhcp_node_t *node, int *status, unsigned int *host,
 
     if (NULL == node)
     {
-        printf("node created\n");
         node = DHCPCreateNode();
         if (NULL == node)
         {
@@ -304,104 +292,3 @@ static ip_t GetServerIP(ip_t net_addr)
 {
     return (net_addr + 1);
 }
-
-static void PrintIP(ip_t ip)
-{
-    printf("%u.%u.%u.%u / %u\n",
-    ip >> 24,
-    (ip << 8) >> 24,
-    (ip << 16) >> 24,
-    (ip << 24) >> 24,
-    ip);
-}
-
-
-/*
-static ip_t GetBroadcastIP(ip_t net_addr, size_t prefix)
-{
-    ip_t ip = {{0, 0, 0, 0}};
-    size_t host_bit_range = 0;
-    unsigned int net_addr_uint = 0;
-    unsigned int host_mask = 0;
-
-    host_bit_range = IP4_BIT_RANGE - prefix;
-    net_addr_uint = IPCharToUInt(net_addr);
-
-    host_mask = (1 << host_bit_range) - 1;
-    
-    return (IPUIntToChar(net_addr_uint | host_mask));
-}
-*/
-
-/*
-static ip_t GetServerIP(ip_t net_addr)
-{
-    unsigned int server_ip_uint = 0;
-
-    server_ip_uint = IPCharToUInt(net_addr) + 1;
-
-    return (IPUIntToChar(server_ip_uint));
-}
-*/
-
-/*
-static unsigned int IPCharToUInt(ip_t ip)
-{
-    unsigned int ip_uint = 0;
-    ip_uint = 
-    (unsigned int)ip.addr[3] << (24 - 1) |
-    (unsigned int)ip.addr[2] << (16 - 1) |
-    (unsigned int)ip.addr[1] << (8 - 1) |
-    (unsigned int)ip.addr[0];  
-
-    return (ip_uint);
-}
-*/
-
-/*
-static ip_t IPUIntToChar(unsigned int ip_uint)
-{
-    ip_t ip = {0, 0, 0, 0};
-    ip.addr[3] = (char)(0xff & (ip_uint >> (24 - 1)));
-    ip.addr[2] = (char)(0xff & (ip_uint >> (16 - 1)));
-    ip.addr[1] = (char)(0xff & (ip_uint >> (8 - 1)));
-    ip.addr[0] = (char)(0xff & ip_uint);
-
-    return (ip);
-}
-*/
-
-/*
-static unsigned int RecAlloc(dhcp_t *dhcp, dhcp_node_t *node, unsigned int host, size_t height)
-{
-    int next_child_side = 0;
-
-    if (0 == height)
-    {
-        return (host);
-    }
-
-    next_child_side = (host & (1 << (height - 1)) != 0);
-
-    if (NULL == node->child[next_child_side])
-    {
-        node->child[next_child_side] = DHCPCreateNode();
-    }
-
-    if (0 != node->child[next_child_side]->is_full)
-    {
-        next_child_side = !next_child_side;
-
-        if (NULL == node->child[next_child_side])
-        {
-            node->child[next_child_side] = DHCPCreateNode();
-        }
-    }
-
-    host = (next_child_side << (height - 1)) & host;
-    host = RecAlloc(dhcp, node->child[next_child_side], host, height - 1);
-    node->is_full = IsFullNode(node, height);
-
-    return (host);
-}
-*/
--- ds/test/dhcp_test.c
+++ ds/test/dhcp_test.c
@@ -15,6 +15,7 @@ void VerifySizet(size_t test,size_t expected, char test_name[]);
 void VerifyVoidptr(void* test, void* expected, char test_name[]);
 void VerifyChar(char test[], char expected[], char test_name[]);
 void VerifyStrncmp(char test[], char expected[], size_t n, char test_name[]);
+static void PrintIP(ip_t ip);
 
 /* API test functions */
 
@@ -75,6 +76,16 @@ void VerifyStrncmp(char test[], char expected[], size_t n, char test_name[])
 	printf(RESET);
 }
 
+static void PrintIP(ip_t ip)
+{
+    printf("%u.%u.%u.%u / %u\n",
+    ip >> 24,
+    (ip << 8) >> 24,
+    (ip << 16) >> 24,
+    (ip << 24) >> 24,
+    ip);
+}
+
 void TestBasic()
 {
 	ip_t net_addr = 0;
@@ -83,15 +94,24 @@ void TestBasic()
 	dhcp_t *dhcp = NULL;
 	ip_t out_ip1 = 0;
 	ip_t expected_out_ip1 = 0;
+	int status = 0;
+	size_t host_range_test = 0, host_range_expected = 0;
+
+	printf("Basic tests:\n");
 
 	/* net_addr = 10.0.2.0, subnet mask = 255.255.255.0 */
 	net_addr |= ((10 << 24) | (0 << 16) | (2 << 8) | 0);
+	printf("Create with network address: ");
+	PrintIP(net_addr);
 	dhcp = DHCPCreate(net_addr, prefix);
 	VerifySizet(DHCPCountFree(dhcp), 256 - 3, "COUNT_FREE AFTER CREATE");
+	host_range_expected = DHCPCountFree(dhcp);
 
 	req_ip1 = 0 | ((10 << 24) | (0 << 16) | (2 << 8) | 31);
 	expected_out_ip1 = 167772703;
 
+	printf("Alloc with required IP: ");
+	PrintIP(req_ip1);
 	VerifyInt(DHCPAlloc(dhcp, req_ip1, &out_ip1), 0, "ALLOC - RETURN VALUE");
 	VerifySizet(out_ip1, expected_out_ip1, "ALLOC - OUT_IP");
 	VerifySizet(DHCPCountFree(dhcp), 256 - 4, "COUNT_FREE AFTER 1 ALLOC");
@@ -148,11 +168,18 @@ void TestBasic()
 	DHCPFree(dhcp, req_ip1);
 	VerifySizet(DHCPCountFree(dhcp), 256 - 3, "COUNT_FREE EMPTY");
 
-	/* TODO:
-	check free works and change size
-	check no more free ips: loop that checks status is success in all iterations
-	and one more check after loop that checks status is no_more_ips
-	*/
+	req_ip1 = 0 | ((10 << 24) | (0 << 16) | (2 << 8) | 2);
+	while (0 == status)
+	{
+		status = DHCPAlloc(dhcp, req_ip1, &out_ip1);
+		if (0 == status)
+		{
+			++host_range_test;
+		}
+	}
+
+	VerifySizet(host_range_test, host_range_expected, "COUNT ALLOCS UNTIL NO MORE AVAILABLE");
+	VerifyInt(status, NO_FREE_IPS, "ALLOC RETURN VALUE - NO FREE IPS");
 
 	DHCPDestroy(dhcp);
 }
