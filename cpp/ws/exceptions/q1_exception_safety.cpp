642ca78c1d5c016571f3594bc642da57bf574dbf
yoni.horovitz <yonihz@gmail.com>
2020-01-13 08:52:49 +0200

    exceptions ws

--- cpp/ws/exceptions/q1_exception_safety.cpp
+++ cpp/ws/exceptions/q1_exception_safety.cpp
@@ -1,14 +1,36 @@
-void Foo(boolean goKaboomEarly)
+#include <iostream>
+
+void Bar(int* arr) {}
+void Fishi() {}
+void Dodo() {}
+
+void Foo(bool goKaboomEarly)
 {
-    int *arr1 = new int[10];
+    int *arr1;
+    try
+    {
+        arr1 = new int[10];
+    }
+    catch(std::bad_alloc)
+    {
+        std::cerr << "Out of memory! exiting."; exit(2);
+    }
 
     if (goKaboomEarly)
     {
         Bar(arr1);
     }
 
-    int *arr2 = new int[100];
-
+    int *arr2;
+    try
+    {
+        arr2 = new int[100];
+    }
+    catch(std::bad_alloc)
+    {
+        std::cerr << "Out of memory! exiting."; exit(2);
+    }
+    
     Fishi();
     Bar(arr2);
     Dodo();
