6dad6b9b33cac9fc031ba7140db271d7c9ee256e
yoni.horovitz <yonihz@gmail.com>
2020-01-15 18:39:10 +0200

    lockguard

--- cpp/ws/lockguard/scope_guard_test.cpp
+++ cpp/ws/lockguard/scope_guard_test.cpp
@@ -1,15 +1,49 @@
 #include <iostream>
+#include <fstream>
 #include <mutex>
 
 #include "scope_guard.hpp"
 
-std::mutex mutex_lock;
+class File
+{
+public:
+    File(std::string filename_) : m_filename(filename_) { }
+    
+    ~File() { };
+
+    void Acquire()
+    {
+        std::cout << "Acquire\n";
+        m_file.open(m_filename.c_str(), std::ofstream::out | std::ofstream::app);
+        
+    }
+
+    void Release()
+    {
+        std::cout << "Release\n";
+        m_file.close();
+    }
+
+    std::fstream& GetFile()
+    {
+        return m_file;
+    }
+
+private:
+    std::fstream m_file;
+    std::string m_filename;
+};
 
 int main () 
 {
+    File file1("my_file.log");
+
     {
         std::cout << "start of scope\n";
-        ilrd::ScopeGuard<std::mutex, &std::mutex::lock, &std::mutex::unlock> sg(mutex_lock);
+        ilrd::ScopeGuard<File, &File::Acquire, &File::Release> sg(file1);
+
+        file1.GetFile() << "Hello\n";
+        file1.GetFile() << "la la la\n";
         std::cout << "end of scope\n";
     }
 }
