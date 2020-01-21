cfede61f2316a5f07b9e9cecd19078d1d479ccab
yoni.horovitz <yonihz@gmail.com>
2020-01-21 23:33:15 +0200

    bits array

--- cpp/ws/BitsArray/bits_array.hpp
+++ cpp/ws/BitsArray/bits_array.hpp
@@ -1,7 +1,11 @@
 #ifndef _ILRD_RD743_BITS_ARRAY_HPP_
 #define _ILRD_RD743_BITS_ARRAY_HPP_
 
+#include <algorithm>
 #include <cstddef>
+#include <cstring>
+
+typedef unsigned long DWORD;
 
 namespace ilrd
 {
@@ -29,6 +33,7 @@ public:
     BitsArray& operator^=(const BitsArray& other_);
 
     bool operator==(const BitsArray& other_);
+    bool operator!=(const BitsArray& other_);
 
     void SetAll(bool val_ = true);
     size_t Count(bool val_ = true) const;
@@ -40,7 +45,7 @@ public:
     bool GetBit(size_t idx_) const;
 
 private:
-    unsigned long m_arr[N/sizeof(unsigned long) + 1];
+    DWORD m_arr[(N / sizeof(DWORD)) + 1 - ((N % sizeof(DWORD)) == 0)];
 
     class BitProxy
     {
@@ -49,7 +54,7 @@ private:
         ~BitProxy();
         BitProxy& operator=(BitProxy& rhs_);
         BitProxy& operator=(bool rhs_);
-        // operator bool() const;
+        operator bool() const;
         // void Flip();
         // bool operator~() const;
     };
@@ -57,128 +62,140 @@ private:
 
 template<size_t N>
 BitsArray<N>::BitsArray(size_t size_)
+    : m_arr() { }
+
+template<size_t N>
+BitsArray<N>::~BitsArray() { }
+
+template<size_t N>
+BitsArray<N>::BitsArray(const BitsArray<N>& other_)
+    : m_arr(other_.m_arr) { }
+
+template<size_t N>
+BitsArray<N>& BitsArray<N>::operator=(const BitsArray<N>& other_)
+{
+    memcpy(&m_arr, &other_.m_arr, sizeof(m_arr));
+}
+
+template<size_t N>
+bool BitsArray<N>::operator[](size_t idx_) const
+{
+    return m_arr[idx_];
+}
+
+template<size_t N>
+typename BitsArray<N>::BitProxy BitsArray<N>::operator[](size_t idx_)
+{
+    return BitProxy(*this, idx_);
+}
+
+template<size_t N>
+BitsArray<N>& BitsArray<N>::operator>>=(size_t n_)
 {
 
 }
 
 template<size_t N>
-BitsArray::~BitsArray()
+BitsArray<N>& BitsArray<N>::operator<<=(size_t n_)
 {
 
 }
 
 template<size_t N>
-BitsArray::BitsArray(const BitsArray& other_)
+BitsArray<N>& BitsArray<N>::operator&=(const BitsArray<N>& other_)
 {
 
 }
 
 template<size_t N>
-BitsArray& BitsArray::operator=(const BitsArray& other_)
+BitsArray<N>& BitsArray<N>::operator|=(const BitsArray<N>& other_)
 {
 
 }
 
 template<size_t N>
-bool BitsArray::operator[](size_t idx_) const
+BitsArray<N>& BitsArray<N>::operator^=(const BitsArray<N>& other_)
 {
 
 }
 
 template<size_t N>
-BitProxy BitsArray::operator[](size_t idx_)
+bool BitsArray<N>::operator==(const BitsArray& other_)
 {
 
 }
 
 template<size_t N>
-BitsArray& BitsArray::operator>>=(size_t n_)
+bool BitsArray<N>::operator!=(const BitsArray& other_)
 {
 
 }
 
 template<size_t N>
-BitsArray& BitsArray::operator<<=(size_t n_)
+void BitsArray<N>::SetAll(bool val_)
 {
 
 }
 
 template<size_t N>
-BitsArray& BitsArray::operator&=(const BitsArray& other_)
+size_t BitsArray<N>::Count(bool val_) const
 {
 
 }
 
 template<size_t N>
-BitsArray& BitsArray::operator|=(const BitsArray& other_)
+BitsArray<N>& BitsArray<N>::ToggleAll()
 {
 
 }
 
 template<size_t N>
-BitsArray& BitsArray::operator^=(const BitsArray& other_)
+BitsArray<N>& BitsArray<N>::ToggleOne(size_t idx_)
 {
 
 }
 
 template<size_t N>
-bool BitsArray::operator==(const BitsArray& other_)
+BitsArray<N>& BitsArray<N>::SetBit(size_t idx_, bool val_)
 {
 
 }
 
 template<size_t N>
-void BitsArray::SetAll(bool val_ = true)
+bool BitsArray<N>::GetBit(size_t idx_) const
 {
 
 }
 
 template<size_t N>
-size_t BitsArray::Count(bool val_ = true) const
+typename BitsArray<N>::BitProxy::BitProxy(BitsArray &org_, size_t idx_)
 {
 
 }
 
 template<size_t N>
-BitsArray& BitsArray::ToggleAll()
+typename BitsArray<N>::BitProxy::~BitProxy()
 {
 
 }
 
 template<size_t N>
-BitsArray& BitsArray::ToggleOne(size_t idx_)
+typename BitsArray<N>::BitProxy& BitsArray<N>::BitProxy::operator=(BitProxy& rhs_)
 {
 
 }
 
 template<size_t N>
-BitsArray& BitsArray::SetBit(size_t idx_, bool val_);
-bool GetBit(size_t idx_) const;
-
-
-BitProxy(BitsArray& org_, size_t idx_)
+typename BitsArray<N>::BitProxy& BitsArray<N>::BitProxy::operator=(bool rhs_)
 {
 
 }
 
-~BitProxy()
+template<size_t N>
+BitsArray<N>::BitProxy::operator bool() const
 {
 
 }
-
-BitProxy& operator=(BitProxy& rhs_)
-{
-
-}
-
-BitProxy& operator=(bool rhs_)
-{
-
-}
-
-// operator bool() const;
-// void Flip();
-// bool operator~() const;
 
 } //namespace ilrd
 
---
+++ cpp/ws/BitsArray/bits_array_test.cpp
@@ -0,0 +1,8 @@
+#include "bits_array.hpp"
+
+using namespace ilrd;
+
+int main()
+{
+    BitsArray<16> ba1(16);
+}
