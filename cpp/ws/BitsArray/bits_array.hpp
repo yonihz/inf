c9931774b2f362587c45f9fe9052280eb71d33e4
yoni.horovitz <yonihz@gmail.com>
2020-01-22 18:51:06 +0200

    bits array

--- cpp/ws/BitsArray/bits_array.hpp
+++ cpp/ws/BitsArray/bits_array.hpp
@@ -5,10 +5,24 @@
 #include <cstddef>
 #include <cstring>
 
+#include <bits/stdc++.h>
+
+#define B2(n) n, n + 1, n + 1, n + 2
+#define B4(n) B2(n), B2(n + 1), B2(n + 1), B2(n + 2)
+#define B6(n) B4(n), B4(n + 1), B4(n + 1), B4(n + 2)
+#define SET_BITS B6(0), B6(1), B6(1), B6(2)
+
+static size_t set_bits_lut[256] = { SET_BITS };
+
 typedef unsigned long DWORD;
 
 namespace ilrd
 {
+
+struct ToggleAll_;
+struct BitwiseAnd_;
+struct BitwiseOr_;
+struct BitwiseXor_;
 
 template<size_t N>
 class BitsArray
@@ -16,7 +30,10 @@ class BitsArray
     class BitProxy;
 
 public:
-    explicit BitsArray(size_t size_);
+    const static DWORD BIT_DWORD = (CHAR_BIT * sizeof(DWORD));
+    const static DWORD N_DWORD = (N/BIT_DWORD) + 1 - ((N%BIT_DWORD) == 0);
+
+    explicit BitsArray();
     ~BitsArray();
     BitsArray(const BitsArray& other_);
 
@@ -45,7 +62,7 @@ public:
     bool GetBit(size_t idx_) const;
 
 private:
-    DWORD m_arr[(N / sizeof(DWORD)) + 1 - ((N % sizeof(DWORD)) == 0)];
+    DWORD m_arr[N_DWORD];
 
     class BitProxy
     {
@@ -55,32 +72,36 @@ private:
         BitProxy& operator=(BitProxy& rhs_);
         BitProxy& operator=(bool rhs_);
         operator bool() const;
-        // void Flip();
-        // bool operator~() const;
+    private:
+        BitsArray *m_org;
+        DWORD m_idx;
     };
 };
 
 template<size_t N>
-BitsArray<N>::BitsArray(size_t size_)
-    : m_arr() { }
+BitsArray<N>::BitsArray()
+    : m_arr() {}
 
 template<size_t N>
 BitsArray<N>::~BitsArray() { }
 
 template<size_t N>
 BitsArray<N>::BitsArray(const BitsArray<N>& other_)
-    : m_arr(other_.m_arr) { }
+    : m_arr()
+{
+    std::copy(other_.m_arr, other_.m_arr + N_DWORD, m_arr);
+}
 
 template<size_t N>
 BitsArray<N>& BitsArray<N>::operator=(const BitsArray<N>& other_)
 {
-    memcpy(&m_arr, &other_.m_arr, sizeof(m_arr));
+    std::copy(other_.m_arr, other_.m_arr + N_DWORD, m_arr);
 }
 
 template<size_t N>
 bool BitsArray<N>::operator[](size_t idx_) const
 {
-    return m_arr[idx_];
+    return GetBit();
 }
 
 template<size_t N>
@@ -104,37 +125,40 @@ BitsArray<N>& BitsArray<N>::operator<<=(size_t n_)
 template<size_t N>
 BitsArray<N>& BitsArray<N>::operator&=(const BitsArray<N>& other_)
 {
-
+    BitwiseAnd_ bitwise_and;
+    std::transform(m_arr, m_arr + N_DWORD, (const_cast<BitsArray<N>&>(other_)).m_arr, m_arr, bitwise_and);
 }
 
 template<size_t N>
 BitsArray<N>& BitsArray<N>::operator|=(const BitsArray<N>& other_)
 {
-
+    BitwiseOr_ bitwise_or;
+    std::transform(m_arr, m_arr + N_DWORD, (const_cast<BitsArray<N>&>(other_)).m_arr, m_arr, bitwise_or);
 }
 
 template<size_t N>
 BitsArray<N>& BitsArray<N>::operator^=(const BitsArray<N>& other_)
 {
-
+    BitwiseXor_ bitwise_xor;
+    std::transform(m_arr, m_arr + N_DWORD, (const_cast<BitsArray<N>&>(other_)).m_arr, m_arr, bitwise_xor);
 }
 
 template<size_t N>
 bool BitsArray<N>::operator==(const BitsArray& other_)
 {
-
+    return (std::equal(m_arr, m_arr + N_DWORD, other_.m_arr));
 }
 
 template<size_t N>
 bool BitsArray<N>::operator!=(const BitsArray& other_)
 {
-
+   return !(std::equal(m_arr, m_arr + N_DWORD, other_.m_arr));
 }
 
 template<size_t N>
 void BitsArray<N>::SetAll(bool val_)
 {
-
+    std::fill(m_arr, m_arr + N_DWORD, ~(DWORD)val_ + 1ul);
 }
 
 template<size_t N>
@@ -146,56 +170,111 @@ size_t BitsArray<N>::Count(bool val_) const
 template<size_t N>
 BitsArray<N>& BitsArray<N>::ToggleAll()
 {
-
+    ToggleAll_ toggle_all;
+    std::for_each(m_arr, m_arr + N_DWORD, toggle_all);
+    return *this;
 }
 
 template<size_t N>
 BitsArray<N>& BitsArray<N>::ToggleOne(size_t idx_)
 {
+    DWORD m = (1 << (idx_ % BIT_DWORD));
 
+    m_arr[idx_ / BIT_DWORD] ^= m;
+
+    return *this;
 }
 
 template<size_t N>
 BitsArray<N>& BitsArray<N>::SetBit(size_t idx_, bool val_)
 {
+    DWORD m = (1 << (idx_ % BIT_DWORD));
+    m_arr[idx_ / BIT_DWORD] &= (~m);
+    m_arr[idx_ / BIT_DWORD] |= (val_ << (idx_ % BIT_DWORD));
 
+    return *this;
 }
 
 template<size_t N>
 bool BitsArray<N>::GetBit(size_t idx_) const
 {
-
+    return (m_arr[idx_ / BIT_DWORD] >> (idx_ % BIT_DWORD)) & 1ul;
 }
 
 template<size_t N>
-typename BitsArray<N>::BitProxy::BitProxy(BitsArray &org_, size_t idx_)
-{
-
-}
+BitsArray<N>::BitProxy::BitProxy(BitsArray &org_, size_t idx_)
+    : m_org(&org_), m_idx(idx_) {}
 
 template<size_t N>
-typename BitsArray<N>::BitProxy::~BitProxy()
-{
-
-}
+BitsArray<N>::BitProxy::~BitProxy() {}
 
 template<size_t N>
 typename BitsArray<N>::BitProxy& BitsArray<N>::BitProxy::operator=(BitProxy& rhs_)
 {
-
+    m_org->SetBit(m_idx, rhs_.m_org->GetBit(rhs_.m_idx));
+    return *this;
 }
 
 template<size_t N>
 typename BitsArray<N>::BitProxy& BitsArray<N>::BitProxy::operator=(bool rhs_)
 {
-
+    m_org->SetBit(m_idx, rhs_);
+    return *this;
 }
 
 template<size_t N>
 BitsArray<N>::BitProxy::operator bool() const
 {
-
+    return m_org->GetBit(m_idx);
 }
+
+struct CountWord
+{
+    void operator() (DWORD &elem)
+    {
+        
+    }
+};
+
+struct CountChar
+{
+    void operator() (char &elem)
+    {
+        
+    }
+};
+
+struct ToggleAll_
+{
+    void operator() (DWORD &elem)
+    {
+        elem ^= (~0);
+    }
+};
+
+struct BitwiseAnd_
+{
+    DWORD &operator() (DWORD &elem1, DWORD &elem2)
+    {
+        return (elem1 &= elem2);
+    }
+};
+
+struct BitwiseOr_
+{
+    DWORD &operator() (DWORD &elem1, DWORD &elem2)
+    {
+        return (elem1 |= elem2);
+    }
+};
+
+struct BitwiseXor_
+{
+    DWORD &operator() (DWORD &elem1, DWORD &elem2)
+    {
+        return (elem1 ^= elem2);
+    }
+};
 
 } //namespace ilrd
 
--- cpp/ws/BitsArray/bits_array_test.cpp
+++ cpp/ws/BitsArray/bits_array_test.cpp
@@ -1,8 +1,141 @@
+#include <iostream>
+
 #include "bits_array.hpp"
+
+void Test1();
+void Test2();
 
 using namespace ilrd;
 
+template<size_t N>
+void PrintBitsArray(BitsArray<N> &ba, DWORD size);
+
 int main()
 {
-    BitsArray<16> ba1(16);
+    // Test1();
+    Test2();
+}
+
+void Test1()
+{
+    std::cout << "BitsArray<80> ba1" << std::endl;
+    BitsArray<80> ba1;
+    BitsArray<80> ba3;
+
+    // std::cout << ba1[1] << std::endl;
+    std::cout << "----- test: SetBit() to 1" << std::endl;
+    std::cout << "ba1.SetBit(1, 1)" << std::endl; ba1.SetBit(1, 1);
+    std::cout << "ba1.SetBit(3, 1)" << std::endl; ba1.SetBit(3, 1);
+    std::cout << "ba1.SetBit(5, 1)" << std::endl; ba1.SetBit(5, 1);
+    std::cout << "ba1.SetBit(65, 1)" << std::endl; ba1.SetBit(65, 1);
+    PrintBitsArray(ba1, 80);
+
+    std::cout << "ba1.GetBit(1) " << ba1.GetBit(1) << std::endl;
+    std::cout << "ba1.GetBit(2) " << ba1.GetBit(2) << std::endl;
+    std::cout << "ba1.GetBit(3) " << ba1.GetBit(3) << std::endl;
+    std::cout << "ba1.GetBit(4) " << ba1.GetBit(4) << std::endl;
+    std::cout << "ba1.GetBit(5) " << ba1.GetBit(5) << std::endl;
+    std::cout << "ba1.GetBit(64) " << ba1.GetBit(64) << std::endl;
+    std::cout << "ba1.GetBit(65) " << ba1.GetBit(65) << std::endl;
+    std::cout << "ba1[65] " << ba1[65] << std::endl;
+
+    std::cout << "----- test: CCtor" << std::endl;
+    std::cout << "BitsArray<80> ba2(ba1)" << std::endl;
+    BitsArray<80> ba2(ba1);
+    PrintBitsArray(ba2, 80);
+
+    std::cout << "----- test: op=" << std::endl;
+    std::cout << "ba3 = ba1" << std::endl;
+    ba3 = ba1;
+    PrintBitsArray(ba3, 80);
+
+    std::cout << "----- test: SetBit() to 0" << std::endl;
+    std::cout << "ba1.SetBit(3, 0)" << std::endl; ba1.SetBit(3, 0);
+    std::cout << "ba1.SetBit(65, 0)" << std::endl; ba1.SetBit(65, 0);
+    PrintBitsArray(ba1, 80);
+    std::cout << "ba1.GetBit(3) " << ba1.GetBit(3) << std::endl;
+    std::cout << "ba1.GetBit(65) " << ba1.GetBit(65) << std::endl;
+    std::cout << "ba1[65] " << ba1[65] << std::endl;
+
+    std::cout << "----- test: ToggleOne()" << std::endl;
+    std::cout << "ba1.GetBit(5) " << ba1.GetBit(5) << std::endl;
+    std::cout << "ba1.GetBit(65) " << ba1.GetBit(65) << std::endl;
+    std::cout << "ba1.ToggleOne(5)" << std::endl; ba1.ToggleOne(5);
+    std::cout << "ba1.ToggleOne(65)" << std::endl; ba1.ToggleOne(65);
+    PrintBitsArray(ba1, 80);
+    std::cout << "ba1.GetBit(5) " << ba1.GetBit(5) << std::endl;
+    std::cout << "ba1.GetBit(65) " << ba1.GetBit(65) << std::endl;
+
+    std::cout << "----- test: ToggleAll()" << std::endl;
+    std::cout << "ba1.GetBit(1) " << ba1.GetBit(1) << std::endl;
+    std::cout << "ba1.GetBit(2) " << ba1.GetBit(2) << std::endl;
+    std::cout << "ba1.GetBit(3) " << ba1.GetBit(3) << std::endl;
+    std::cout << "ba1.GetBit(4) " << ba1.GetBit(4) << std::endl;
+    std::cout << "ba1.GetBit(5) " << ba1.GetBit(5) << std::endl;
+    std::cout << "ba1.ToggleAll()" << std::endl; ba1.ToggleAll();
+    PrintBitsArray(ba1, 80);
+    std::cout << "ba1.GetBit(1) " << ba1.GetBit(1) << std::endl;
+    std::cout << "ba1.GetBit(2) " << ba1.GetBit(2) << std::endl;
+    std::cout << "ba1.GetBit(3) " << ba1.GetBit(3) << std::endl;
+    std::cout << "ba1.GetBit(4) " << ba1.GetBit(4) << std::endl;
+    std::cout << "ba1.GetBit(5) " << ba1.GetBit(5) << std::endl;
+    std::cout << "----- test: op[]" << std::endl;  
+}
+
+void Test2()
+{
+    std::cout << "BitsArray<80> ba1, ba2" << std::endl;
+    BitsArray<80> ba1;
+    BitsArray<80> ba2;
+    PrintBitsArray(ba1, 80);
+    PrintBitsArray(ba2, 80);
+
+    std::cout << "ba1.SetBit(1, 1)" << std::endl; ba1.SetBit(1, 1);
+    std::cout << "ba1.SetBit(3, 1)" << std::endl; ba1.SetBit(3, 1);
+    std::cout << "ba1.SetBit(5, 1)" << std::endl; ba1.SetBit(5, 1);
+    std::cout << "ba1.SetBit(65, 1)" << std::endl; ba1.SetBit(65, 1);
+    PrintBitsArray(ba1, 80);
+
+    std::cout << "ba1.GetBit(1) " << ba1.GetBit(1) << std::endl;
+    std::cout << "ba1.GetBit(2) " << ba1.GetBit(2) << std::endl;
+    std::cout << "ba1.GetBit(3) " << ba1.GetBit(3) << std::endl;
+    std::cout << "ba1.GetBit(4) " << ba1.GetBit(4) << std::endl;
+    std::cout << "ba1.GetBit(5) " << ba1.GetBit(5) << std::endl;
+    std::cout << "ba1.GetBit(64) " << ba1.GetBit(64) << std::endl;
+    std::cout << "ba1.GetBit(65) " << ba1.GetBit(65) << std::endl;
+
+    std::cout << "ba2 == ba1 " << (ba2 == ba1) << std::endl;
+    std::cout << "ba2 != ba1 " << (ba2 != ba1) << std::endl;
+    std::cout << "----- test: ba2 =| ba1" << std::endl;
+    ba2 |= ba1;
+    PrintBitsArray(ba2, 80);
+
+    std::cout << "ba2.GetBit(1) " << ba2.GetBit(1) << std::endl;
+    std::cout << "ba2.GetBit(2) " << ba2.GetBit(2) << std::endl;
+    std::cout << "ba2.GetBit(3) " << ba2.GetBit(3) << std::endl;
+    std::cout << "ba2.GetBit(4) " << ba2.GetBit(4) << std::endl;
+    std::cout << "ba2.GetBit(5) " << ba2.GetBit(5) << std::endl;
+    std::cout << "ba2.GetBit(64) " << ba2.GetBit(64) << std::endl;
+    std::cout << "ba2.GetBit(65) " << ba2.GetBit(65) << std::endl;
+
+    std::cout << "ba2 == ba1 " << (ba2 == ba1) << std::endl; 
+    std::cout << "ba2 != ba1 " << (ba2 != ba1) << std::endl;
+
+    std::cout << "----- test: ba2.SetAll(true)" << std::endl;
+    ba2.SetAll(true);
+    PrintBitsArray(ba2, 80);
+    std::cout << "----- test: ba2.SetAll(false)" << std::endl;
+    ba2.SetAll(false);
+    PrintBitsArray(ba2, 80);
+}
+
+template<size_t N>
+void PrintBitsArray(BitsArray<N> &ba, DWORD size)
+{
+    for (size_t i = 0; i < size; ++i)
+    {
+        std::cout << ba.GetBit(i);
+    }
+
+    std::cout << std::endl;
 }
