78307b1fd6ebc9bf9e60e2aed072939c439b2c21
yoni.horovitz <yonihz@gmail.com>
2020-02-25 05:48:51 +0200

    ext2

--- cpp/ext2/ext2ex.c
+++ cpp/ext2/ext2ex.c
@@ -14,6 +14,8 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <ext2fs/ext2_fs.h>
+#include <string.h>
+
 
 void PrintSuperBlock(char *device_name);
 void PrintGroupDesc(char *device_name);
@@ -29,7 +31,9 @@ int main()
 
 void PrintSuperBlock(char *device_name)
 {
+    printf("-----\n");
     printf("super_block info:\n");
+    printf("-----\n");
     int fd;
     ssize_t status;
     fd = open(device_name, O_RDONLY);
@@ -52,15 +56,19 @@ void PrintSuperBlock(char *device_name)
     }
 
     printf("super_block: s_magic = %x\n", sb->s_magic);
-    printf("super_block: s_block_group_nr = %lu\n", sb->s_block_group_nr);
-    printf("super_block: s_blocks_count = %lu\n", sb->s_blocks_count);
-    printf("super_block: s_log_block_size = %lu\n", sb->s_log_block_size);
-
+    printf("super_block: s_inodes_count = %d\n", sb->s_inodes_count);
+    printf("super_block: s_blocks_count = %d\n", sb->s_blocks_count);
+    printf("super_block: s_first_data_block = %d\n", sb->s_first_data_block);
+    printf("super_block: s_log_block_size = %d (%d)\n", sb->s_log_block_size, 1 << (10 + sb->s_log_block_size)); /* Block size is 2 ^ (10 + s_log_block_size) */
+    printf("super_block: s_first_ino = %u\n", sb->s_first_ino);
+    printf("super_block: s_inode_size = %u\n", sb->s_inode_size);
 }
 
 void PrintGroupDesc(char *device_name)
 {
-    printf("super_block info:\n");
+    printf("-----\n");
+    printf("group_desc info:\n");
+    printf("-----\n");
     int fd;
     ssize_t status;
     fd = open(device_name, O_RDONLY);
@@ -82,6 +90,35 @@ void PrintGroupDesc(char *device_name)
         return;
     }
 
-    printf("ext2_group_desc: bg_used_dirs_count = %lu\n", gb->bg_used_dirs_count);
-    printf("ext2_group_desc: bg_free_inodes_count = %lu\n", gb->bg_free_inodes_count);
+    printf("ext2_group_desc: bg_inode_table = %x (x) / %d (d)\n", gb->bg_inode_table, gb->bg_inode_table);
+
+    printf("-----\n");
+    printf("inode info:\n");
+    printf("-----\n");
+
+    struct ext2_inode *ino = malloc(sizeof(struct ext2_inode));
+
+    lseek(fd, 1024 * 258 + 1 * 128, SEEK_SET); /* root dir inode */
+    status = read(fd, ino, sizeof(struct ext2_inode));
+
+    if (-1 == status)
+    {
+        perror("read");
+        return;
+    }
+
+    printf("ext2_inode: i_size = %d\n", ino->i_size);
+    printf("ext2_inode: i_block = %d\n", ino->i_block[0]);
+
+    struct ext2_dir_entry_2 *dir = malloc(sizeof(struct ext2_dir_entry_2));
+
+    lseek(fd, 1024 * ino->i_block[0], SEEK_SET); /* root dir data */
+    status = read(fd, dir, sizeof(struct ext2_dir_entry_2));
+
+    printf("ext2_dir_entry: inode = %u\n", dir->inode);
+    printf("ext2_dir_entry: file_type = %d\n", dir->file_type);
+    char *file_name = (char*)malloc(sizeof(dir->name_len));
+    strcpy(file_name, dir->name);
+    printf("ext2_dir_entry: name = %s\n", file_name);
+
 }
